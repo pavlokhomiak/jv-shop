0d0c4488a3fd69243435336c239876c7ddfd02b8
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-09-16 08:24:54 +0300

    hw29 in progress

---
+++ src/main/java/mate/academy/web/filter/AuthorizationFilter.java
@@ -0,0 +1,34 @@
+package mate.academy.web.filter;
+
+import mate.academy.model.Role;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AuthorizationFilter implements Filter {
+    private Map<String, List<Role.RoleName>> protectedUrls = new HashMap<>();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        protectedUrls.put("/users/all", List.of(Role.RoleName.ADMIN));
+        protectedUrls.put("/order/complete", List.of(Role.RoleName.USER));
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
