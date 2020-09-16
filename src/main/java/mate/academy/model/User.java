02de351c28f7a219857ebccea11876327a000b17
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-09-16 16:05:33 +0300

    Implement AuthorizationFilter

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -2,12 +2,15 @@ package mate.academy.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class User {
     private Long id;
     private String name;
     private String login;
     private String password;
+    private List<Role> roles;
 
     public User(String name, String login, String password) {
         this.name = name;
--- src/main/java/mate/academy/web/filter/AuthorizationFilter.java
+++ src/main/java/mate/academy/web/filter/AuthorizationFilter.java
@@ -1,6 +1,9 @@
 package mate.academy.web.filter;
 
+import mate.academy.lb.Injector;
 import mate.academy.model.Role;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,12 +11,20 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AuthorizationFilter implements Filter {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final String USER_ID = "user_id";
+    private final UserService userService =
+            (UserService) injector.getInstance(UserService.class);
     private Map<String, List<Role.RoleName>> protectedUrls = new HashMap<>();
 
     @Override
@@ -25,10 +36,43 @@ public class AuthorizationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
                          FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        String requestedUrl = req.getServletPath();
+        if (protectedUrls.get(requestedUrl) == null) {
+            filterChain.doFilter(req, resp);
+            return;
+        }
 
+        Long userId = (Long) req.getSession().getAttribute(USER_ID);
+        if (userId == null || userService.get(userId) == null) {
+            resp.sendRedirect(req.getContextPath() + "/login");
+            return;
+        }
+
+        User user = userService.get(userId);
+
+        if (isAuthorized(user, protectedUrls.get(requestedUrl))) {
+            filterChain.doFilter(req, resp);
+            return;
+        } else {
+            req.getRequestDispatcher("/WEB-INF/views/accessDenied.jsp").forward(req, resp);
+            return;
+        }
     }
 
     @Override
     public void destroy() {
+    }
+
+    private boolean isAuthorized(User user, List<Role.RoleName> authorizedRoles) {
+        for (Role.RoleName authorizedRole : authorizedRoles) {
+            for (Role userRole : user.getRoles()) {
+                if (authorizedRole.equals(userRole.getRoleName())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
