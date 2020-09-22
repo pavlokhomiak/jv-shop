a0910184fe0397bdd54f2944c596293644c19057
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-09-22 15:09:48 +0300

    Add sql commands for change columns

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -5,3 +5,7 @@ CREATE TABLE `internet_shop`.`products` (
   `deleted` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`product_id`))
 ENGINE = InnoDB;
+
+ALTER TABLE products Change productname product_name VARCHAR(225)
+
+ALTER TABLE products Change productprice product_price DECIMAL(7,2)
